41c629022f82c48afea42caadc0eb5e93d80ee4c
Kluchnikova Maria <kluchnikova@mail.ru>
2022-03-10 00:00:20 +0300

    add cli.py and update brain_games/scripts/brain_games.py

---
+++ brain_games/cli.py
@@ -0,0 +1,6 @@
+impotr prompt
+
+def welcome_user():
+    name = prompt.string("May I have yuor name? ")
+    print("Hallo, " + "name")
+    
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 
+from cli import welcome_user
+
 def main():
     print("Welcome to the Brain Games!")
+    
+welcom_user()
 
 if __name__ == "__main__":
     main()
+
