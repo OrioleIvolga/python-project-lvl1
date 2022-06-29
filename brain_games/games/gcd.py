22e4cf3970a85affdb5cc758b937b811cbe6be20
Kluchnikova Maria <kluchnikova@mail.ru>
2022-06-29 20:08:17 +0300

    Правим по замечаниям 4

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -9,7 +9,7 @@ def gcd():
     random_a = randint(1, 100)
     random_b = randint(1, 100)
     gcd = math.gcd(random_a, random_b)
-    question = (f"`{random_a}` + `{random_b}`")
+    question = (f"{random_a} + {random_b}")
     correct_answer = str(gcd)
     return (question, correct_answer)
 
